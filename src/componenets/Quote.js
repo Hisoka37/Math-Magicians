2715cabedfbafbe86c6590e68873fada57ec0219
Walid Kabboussa <walidkabou@gmail.com>
2023-03-24 16:17:11 +0000

    Add loading and error state

--- src/componenets/Quote.js
+++ src/componenets/Quote.js
@@ -3,6 +3,8 @@ import './quote.css';
 
 const Quote = () => {
   const [quotes, setQuotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchQuotes = async () => {
@@ -16,18 +18,32 @@ const Quote = () => {
             'X-Api-Key': API_KEY,
           },
         });
+        if (!response.ok) {
+          throw new Error('Response was not ok 🤒');
+        }
         const data = await response.json();
         setQuotes(data);
+        setLoading(false);
       } catch (error) {
         console.error('Error:', error);
+        setError(error);
+        setLoading(false);
       }
     };
     fetchQuotes();
   }, []);
 
+  if (loading) {
+    return <div>Hi, Loading ...</div>;
+  }
+
+  if (error) {
+    return <div> Oops, something has gone wrong, Please refresh the page</div>;
+  }
+
   return (
     <div className="quote">
-      <h1>Quotes</h1>
+      <h1>Quote of The Day</h1>
       <ul>
         {quotes.map((quote) => (
           <li key={quote.id}>{quote.quote}</li>
